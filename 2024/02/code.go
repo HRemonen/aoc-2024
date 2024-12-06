545178c9bd199c7f4e7bf507cd9c83775c465746
Henri Remonen <henri@remonen.fi>
2024-12-06 09:23:35 +0200

    feat: exercise 2 part 1

--- 2024/02/code.go
+++ 2024/02/code.go
@@ -1,11 +1,62 @@
 package main
 
 import (
+	"math"
+	"strconv"
+	"strings"
+
 	"github.com/jpillora/puzzler/harness/aoc"
 )
 
 func main() {
 	aoc.Harness(run)
+}
+
+func isIncreasing(input []string) bool {
+	for i := 1; i < len(input); i++ {
+		prev, _ := strconv.Atoi(string(input[i-1]))
+		curr, _ := strconv.Atoi(string(input[i]))
+		if prev >= curr {
+			return false
+		}
+	}
+	return true
+}
+
+// Check if the levels are strictly decreasing
+func isDecreasing(input []string) bool {
+	for i := 1; i < len(input); i++ {
+		prev, _ := strconv.Atoi(string(input[i-1]))
+		curr, _ := strconv.Atoi(string(input[i]))
+		if prev <= curr {
+			return false
+		}
+	}
+	return true
+}
+
+// The Red-Nosed reactor safety systems can only tolerate levels that are
+// either gradually increasing or gradually decreasing.
+// So, a report only counts as safe if both of the following are true:
+//
+// - The levels are either all increasing or all decreasing.
+// - Any two adjacent levels differ by at least one and at most three.
+func checkRules(input []string) bool {
+	if !isIncreasing(input) && !isDecreasing(input) {
+		return false
+	}
+
+	for i := 1; i < len(input); i++ {
+		prev, _ := strconv.Atoi(string(input[i-1]))
+		curr, _ := strconv.Atoi(string(input[i]))
+
+		diff := math.Abs(float64(prev - curr))
+		if diff < 1 || diff > 3 {
+			return false
+		}
+	}
+
+	return true
 }
 
 // on code change, run will be executed 4 times:
@@ -15,10 +66,17 @@ func main() {
 // 4. with: true (part2), and user input
 // the return value of each run is printed to stdout
 func run(part2 bool, input string) any {
+	result := 0
+
+	for _, line := range strings.Split(input, "\n") {
+		if checkRules(strings.Fields(line)) {
+			result++
+		}
+	}
 	// when you're ready to do part 2, remove this "not implemented" block
 	if part2 {
 		return "not implemented"
 	}
 	// solve part 1 here
-	return 42
+	return result
 }
