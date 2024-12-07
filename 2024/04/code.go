784f84ebaa45803a3250466aa0794465707cded8
Henri Remonen <henri@remonen.fi>
2024-12-07 09:39:12 +0200

    feat: day 4 part 1

--- 2024/04/code.go
+++ 2024/04/code.go
@@ -1,11 +1,56 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/jpillora/puzzler/harness/aoc"
 )
 
 func main() {
 	aoc.Harness(run)
+}
+
+var directions = [][2]int{
+	{-1, 0},  // up
+	{1, 0},   // down
+	{0, -1},  // left
+	{0, 1},   // right
+	{-1, -1}, // top-left diagonal
+	{-1, 1},  // top-right diagonal
+	{1, -1},  // bottom-left diagonal
+	{1, 1},   // bottom-right diagonal
+}
+
+func solve(input []string) int {
+	word := "XMAS"
+	wordLength := len(word)
+	result := 0
+
+	for row := range input {
+		for col := range input[row] {
+			for _, dir := range directions {
+				match := true
+				for i := 0; i < wordLength; i++ {
+					newRow := row + i*dir[0]
+					newCol := col + i*dir[1]
+
+					if newRow < 0 || newRow >= len(input) || newCol < 0 || newCol >= len(input[row]) {
+						match = false
+						break
+					}
+					if input[newRow][newCol] != word[i] {
+						match = false
+						break
+					}
+				}
+				if match {
+					result++
+				}
+			}
+		}
+	}
+
+	return result
 }
 
 // on code change, run will be executed 4 times:
@@ -15,10 +60,14 @@ func main() {
 // 4. with: true (part2), and user input
 // the return value of each run is printed to stdout
 func run(part2 bool, input string) any {
+	rows := strings.Split(input, "\n")
+
 	// when you're ready to do part 2, remove this "not implemented" block
 	if part2 {
 		return "not implemented"
 	}
-	// solve part 1 here
-	return 42
+
+	result1 := solve(rows)
+
+	return result1
 }
